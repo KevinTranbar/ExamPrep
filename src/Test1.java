7b0575c67cf7dbcaeefd49b3c45ae7f81dae3bda
KevinTranbar <kt222sx@student.lnu.se>
2025-10-21 10:45:18 +0200

    ExamPrep

--- src/Test1.java
+++ src/Test1.java
@@ -1,2 +1,7 @@
 public class Test1 {
+    public static void main(String[] args) {
+        int i = 0;
+        int j = i++ + i*5; //0 först läses in och sen ökar i med ett, och eftersom i * 5 är efter i++, kommer ökningen appliceras till i * 5
+        System.out.println(j);
+    }
 }
